aaa2279dcf56a3f077469e9c201e24e3f891167e
PicotDesBois <79858497+PicotDesBois@users.noreply.github.com>
2022-04-04 15:22:52 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include "Avion.h"
+
+using namespace std;
+void ChargementAvions(std::vector<Avion*>m_listeAvions);
+
+int main()
+{
+    std::vector<Avion*>m_listeAvions;
+    ChargementAvions(m_listeAvions);
+
+    return 0;
+}
+void ChargementAvions(std::vector<Avion*>m_listeAvions)
+{
+    int nombre;
+    std::string nomFichier="Avion.txt";
+
+    /***************************** Ouverture du fichier ******************************/
+    std::ifstream ifs{nomFichier};
+    if (!ifs)
+        throw std::runtime_error( "Impossible d'ouvrir en lecture " + nomFichier );
+    /*******************************************************************************/
+
+    /***************************** Lecture de le nombre d'avions ********************/
+    ifs >> nombre;
+    if ( ifs.fail() )
+        throw std::runtime_error("Probleme lecture nombre");
+    /*******************************************************************************/
+
+    /******************** Ajout des sucesseurs et de la distance avec le sommet *******************/
+    int reservoir,consommation,identifiant;
+    std::string type;
+    for (int i=0;i<nombre;++i){
+        ifs>>type>>identifiant>>consommation>>reservoir;
+        if ( ifs.fail() )
+        throw std::runtime_error("Probleme lecture arc");
+        m_listeAvions.push_back(new Avion{type,identifiant,reservoir});
+    }
+    /*******************************************************************************/
+    for(auto elem:m_listeAvions)
+        elem->afficher();
+}
+
+
